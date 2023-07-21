ac34463235eedde3d57cdb7c9465bf1ac359c5ed
Prograweb2023 <62483268+nxonfec@users.noreply.github.com>
2023-07-20 23:00:53 -0500

    Add files via upload

--- database/database.js
+++ database/database.js
@@ -1,6 +1,12 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize(process.env.PGDATABASE, process.env.PGUSER, process.env.PGPASSWORD, {
-   host: process.env.PGHOST,
-   dialect: "postgres"
-});
+export const sequelize = new Sequelize(process.env.DATABASE_URL,
+   {
+      dialect: "postgres",
+      dialectOptions: {
+         ssl: {
+            require: true,
+            rejectUnauthorized: false
+         }
+      }
+   });
